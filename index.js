9f784a5088a507d1443ee33cb74acc077ac5ea2f
said baradai <saitbaradai@gmail.com>
2023-04-13 09:23:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee={
+    name:"said baradai",
+    streetAddress:"Kutahya/Turkey"
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+
+    const newObj={...employee}
+    newObj[key]=value
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj=employee
+    newObj[key]=value
+    return newObj
+}
+
+function deleteFromEmployeeByKey(employee, key){
+
+    const newObj={...employee}
+    delete newObj[key]
+
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    const newObj=employee
+    delete employee[key]
+
+    return newObj
+}
